ea1a44d80bdad056c8fdefa13a7f0d88eeb4e29c
avinashvalkya <60166977+avinashvalkya@users.noreply.github.com>
2023-03-31 18:51:46 +0530

    Add files via upload

--- wrapper_dag.py
+++ wrapper_dag.py
@@ -4,9 +4,10 @@ from airflow.providers.google.cloud.hooks.bigquery import BigQueryHook
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+from time import sleep
 
 default_args = {
-    'start_date': datetime(2023, 3, 28)
+    'start_date': datetime(2023, 3, 30)
 }
 
 dag = DAG(
@@ -37,34 +38,25 @@ start_task = DummyOperator(
 tasks = {}
 branches_interface = {}
 branches_folder_path = {}
+new_tasks = {}
 
 # Define the Python callable to create the file_name tasks
-# def create_file_name_task(ti, **kwargs):
-#     file_name = ti.xcom_pull(task_ids='file_name_task', key='file_name')
-#     task_name = f'process_{file_name}'
-#     bash_command = f'echo {task_name}'
-#     task = BashOperator(
-#         task_id=task_name,
-#         bash_command=bash_command,
-#         dag=dag
-#     )
-#     tasks[task_name] = task
-#     return task_name
-
-def create_file_name_task(file_name):
-    # file_name = ti.xcom_pull(task_ids='file_name_task', key='file_name')
-    # task_name = f'process_{file_name}'
-    # bash_command = f'echo {task_name}'
+def create_file_name_task(file_name, **kwargs):
+    task_ids = []
     print(f'The value of my_param is: {file_name}')
-
-    # task = BashOperator(
-    #     task_id=task_name,
-    #     bash_command=bash_command,
-    #     dag=dag
-    # )
-    # tasks[task_name] = task
-
-    return file_name
+    task = BashOperator(
+        task_id=f'new_task_{file_name}',
+        bash_command='echo "This task runs after the file_name_task"',
+        dag=dag
+    )
+    task_ids.append(task.task_id)
+    task = BashOperator(
+        task_id=f'new_task_{file_name}_x',
+        bash_command='echo "This task runs after the file_name_task with suffix _x"',
+        dag=dag
+    )
+    task_ids.append(task.task_id)
+    return task_ids
 
 # Loop through the results and create the interface branches
 for interface in results['INTF_NAME'].unique():
@@ -89,27 +81,20 @@ for interface in results['INTF_NAME'].unique():
         # Loop through the results for this interface and folder_path and create the file_name tasks
         for index, row in results[(results['INTF_NAME']==interface) & (results['folder_path']==folder_path)].iterrows():
             file_name = row['file_name']
-            file_name_task_id = f'file_name_task_{interface}_{folder_path}_{file_name}'
-            file_name_task = PythonOperator(
-                task_id=file_name_task_id,
-                provide_context=True,
-                python_callable=create_file_name_task,
-                op_kwargs={'file_name': file_name},
-                dag=dag
-            )
-            tasks[file_name_task_id] = file_name_task
-            branch_folder_path >> file_name_task
-
-# Define the final task to end the DAG
-end_task = BashOperator(
-    task_id='end_task',
-    bash_command='echo "DAG completed."',
-    dag=dag
-)
-
-# Set the final task dependency on all the dynamic tasks created
-for task_name in tasks:
-    tasks[task_name] >> end_task
+            # file_name_task_id = f'file_name_task_{interface}_{folder_path}_{file_name}'
+            # file_name_task = PythonOperator(
+            #     task_id=file_name_task_id,
+            #     provide_context=True,
+            #     python_callable=create_file_name_task,
+            #     op_kwargs={'file_name': file_name},
+            #     dag=dag
+            # )
+            # tasks[file_name_task_id] = file_name_task.output
+            # branch_folder_path >> file_name_task
+            task_ids = create_file_name_task(file_name)
+            for task_id in task_ids:
+                task = dag.get_task(task_id)
+                branch_folder_path >> task
 
 if __name__ == '__main__':
     dag.cli()
