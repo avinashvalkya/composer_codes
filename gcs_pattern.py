87f359456d81acc37509803c04e6780bcdb5fdc0
avinashvalkya <60166977+avinashvalkya@users.noreply.github.com>
2023-04-18 14:03:10 +0530

    Create gcs_pattern.py

---
+++ gcs_pattern.py
@@ -0,0 +1,20 @@
+from google.cloud import storage
+from google.cloud import bigquery
+
+# Set up GCS client and bucket name
+storage_client = storage.Client()
+bucket_name = 'your_bucket_name'
+
+# Set up BigQuery client and table name
+bigquery_client = bigquery.Client()
+table_name = 'your_dataset.your_table_name'
+
+# Get list of files in bucket
+blobs = storage_client.list_blobs(bucket_name)
+
+# Loop through blobs and check prefix against table
+for blob in blobs:
+    for row in bigquery_client.list_rows(table_name):
+        if blob.name.startswith(row.prefix):
+            system_name = row.system_name
+            print(f"The file {blob.name} belongs to system {system_name}")
