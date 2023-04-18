a59afd8f90c64a7e39ba0da811d8600a7e3da2d8
avinashvalkya <60166977+avinashvalkya@users.noreply.github.com>
2023-04-18 14:05:52 +0530

    Update gcs_pattern.py

--- gcs_pattern.py
+++ gcs_pattern.py
@@ -9,12 +9,16 @@ bucket_name = 'your_bucket_name'
 bigquery_client = bigquery.Client()
 table_name = 'your_dataset.your_table_name'
 
+# Set the name of the file you're looking for
+file_name = 'your_file_name.txt'
+
 # Get list of files in bucket
 blobs = storage_client.list_blobs(bucket_name)
 
 # Loop through blobs and check prefix against table
 for blob in blobs:
-    for row in bigquery_client.list_rows(table_name):
-        if blob.name.startswith(row.prefix):
-            system_name = row.system_name
-            print(f"The file {blob.name} belongs to system {system_name}")
+    if blob.name == file_name:
+        for row in bigquery_client.list_rows(table_name):
+            if blob.name.startswith(row.prefix):
+                system_name = row.system_name
+                print(f"The file {blob.name} belongs to system {system_name}")
